b31853a2016b418a5fd3e427c4a4575a8ba7245a
hoanghaiwork06-blin <hoanghai.work06@gmail.com>
2026-09-08 17:29:05 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,9 +1,14 @@
 # @echo off
 # title commitScript
 
+# git status
+# git add .
+# git commit -m "$1"
+# git push origin main
+
+git add --sparse .
 git status
-git add .
-git commit -m "$1"
+git commit -m "Upload 180 new images"
 git push origin main
 
 # pause
